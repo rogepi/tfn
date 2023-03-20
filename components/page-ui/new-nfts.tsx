61b5ae82aead1b3c97143ee7d34da466d8790107
rogepi <zzr1119@outlook.com>
2023-03-20 09:37:21 +0800

    ✨ feat: update

--- components/page-ui/new-nfts.tsx
+++ components/page-ui/new-nfts.tsx
@@ -8,7 +8,7 @@ const NewNFTs = ({ listing }: { listing: INFT[] }) => {
       <div className="text-3xl">New NFTs</div>
       <div className="my-8 flex  gap-5">
         {listing.map(item =>
-          <Link href={`/nft/${item.id}`} className="flex h-60 w-44 flex-col items-center justify-center gap-3 overflow-hidden rounded-md border shadow-md hover:border-gray-300" key={item.id}>
+          <Link href={`/nft/${item.tokenId}`} className="flex h-60 w-44 flex-col items-center justify-center gap-3 overflow-hidden rounded-md border shadow-md hover:border-gray-300" key={item.id}>
             <div className="relative block h-full w-full">
               <Image src={item.image as string} alt={item.name as string} fill sizes="100" />
             </div>
