35ef5c60a0b544a8ae9596659ab687c81d0c7dbb
rogepi <zzr1119@outlook.com>
2023-04-02 19:16:13 +0800

    ✨ feat: update

--- components/page-ui/nft-dropdown-menu.tsx
+++ components/page-ui/nft-dropdown-menu.tsx
@@ -1,7 +1,5 @@
+import { Fragment } from 'react'
 import { Menu, Transition } from '@headlessui/react'
-
-import React from 'react'
-
 
 export interface INFTDropDownMenuItem {
   label: string | JSX.Element
@@ -20,19 +18,28 @@ const NFTDropDownMenu = ({ children, menu }: INFTDropDownMenu) => {
         <Menu.Button >
           {children}
         </Menu.Button>
-
-        <Menu.Items className='absolute right-0 top-9 z-50 inline-block
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className='absolute right-0 top-9 z-50 inline-block
          w-32 divide-y overflow-hidden rounded-md border bg-white font-semibold shadow dark:bg-gray-900'>
-          {
-            menu.map((item, index) => (
-              <Menu.Item key={index}>
-                <button onClick={item.onClick} className="w-full bg-white p-1 outline-none hover:bg-blue-500 hover:text-white">
-                  {item.label}
-                </button>
-              </Menu.Item>
-            ))
-          }
-        </Menu.Items>
+            {
+              menu.map((item, index) => (
+                <Menu.Item key={index}>
+                  <button onClick={item.onClick} className="w-full bg-white p-1 outline-none hover:bg-blue-500 hover:text-white">
+                    {item.label}
+                  </button>
+                </Menu.Item>
+              ))
+            }
+          </Menu.Items>
+        </Transition>
       </Menu>
     </div>
   )
