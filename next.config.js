ce3d0b80dc3a4b24dcf94f664dfd7a16c128f859
rogepi <zzr1119@outlook.com>
2023-04-17 00:24:19 +0800

    ✨ feat: update image host

--- next.config.js
+++ next.config.js
@@ -2,14 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: '',
-  images:{
-    remotePatterns:[
+  images: {
+    remotePatterns: [
       {
-        protocol:'https',
-        hostname:'gateway.ipfscdn.io'
-      }
-    ]
-  }
+        protocol: 'https',
+        hostname: 'ipfs.thirdwebcdn.com/ipfs',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
